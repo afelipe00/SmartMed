d4b1d622c82635632ad233e2a7be4facdd3d5a09
Felipe Díaz <47798722+afelipe00@users.noreply.github.com>
2020-11-10 00:28:37 -0500

    ci: :construction: integration with vue

---
+++ src/components/Users.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">    
+        <h3>Users:</h3>
+        <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Id</th>
+                        <th scope="col">name</th>
+                        <th scope="col">email</th>
+                        <th scope="col">country</th>
+                    </tr>
+                </thead>
+        </table>
+    </div>
+</template>
+
+<script>
+
+
+
+export default {
+    name: 'Users',
+}
+</script>
+
+<style>
+    h3{
+        margin-bottom: 5%;
+    }
+</style>
